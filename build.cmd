3f4d43333a6a44a130ae92dc9f146e80ceac1419
Griffin Yourick <gryphon92@gmail.com>
2025-08-16 11:13:46 -0400

    build.cmd for even more symlink abuse

---
+++ build.cmd
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+bash release.sh -dz
+
+:: Function to create directory structure in .release
+for /f "delims=" %%i in ('dir /b /s /a-d *.lua ^| findstr /v .release') do (
+    set "file=%%i"
+    set "relpath=!file:%cd%\=!"
+    set "targetdir=.release\LavUI\!relpath!"
+
+    :: Get directory part of the target path
+    for %%j in ("!targetdir!") do set "targetdir=%%~dpj"
+
+    :: Create directory structure if it doesn't exist
+    if not exist "!targetdir!" mkdir "!targetdir!"
+
+    :: Create symbolic link
+    set "target=.release\LavUI\!relpath!"
+    if exist "!target!" del "!target!"
+    mklink "!target!" "!file!"
+)
